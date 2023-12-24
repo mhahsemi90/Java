078c1be5c23c7c5b3feebbf42530f557da4f4bd4
a.shahravi@fanap.ir <a.shahravi@fanap.ir>
2023-12-24 15:29:30 +0330

    first commit for project

---
+++ src/test/java/com/fanap/hcm/core/hcmcore/HcmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanap.hcm.core.hcmcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcmCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
